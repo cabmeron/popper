39f5038a4c6e40ad03c49d2223d202bb488b3170
cabmeron <116330722+cabmeron@users.noreply.github.com>
2025-06-27 23:46:40 -0700

    feature: first enemy traversal and bg image integration

--- src/game.py
+++ src/game.py
@@ -1,5 +1,7 @@
-import pygame
 import os
+import pygame
+import random
+
 
 def update_vector_cluster_position_up(cluster, new_pos, dt):
     for vec in cluster:
@@ -39,6 +41,9 @@ def main():
     running = True
     dt = 0
 
+    # CORE SCENE IMAGES
+    background_img = pygame.transform.scale(pygame.image.load("assets/art/background.png"), (1280, 720))
+
     # VECTORS
     ball_1 = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     ball_2 = pygame.Vector2(screen.get_width() / 2 + 45, screen.get_height() / 2)
@@ -46,6 +51,9 @@ def main():
     ball_4 = pygame.Vector2(screen.get_width() / 2 , screen.get_height() / 2 + 45)
     ball_5 = pygame.Vector2(screen.get_width() / 2 , screen.get_height() / 2 - 45)
     ball_cluster = [ball_1, ball_2, ball_3, ball_4, ball_5]
+
+
+    target_1 = pygame.Vector2(0, random.randint(20, screen.get_height() - 20))
 
     # GAME AUDIO
     pygame.mixer.init(48000, -16, 1, 1024)
@@ -63,6 +71,7 @@ def main():
     shot_positions = [ gun_shot1, gun_shot2, gun_shot3, gun_shot4, gun_shot5, gun_shot6 ]
     shot_index = 0
     num_shots = len(shot_positions)
+    
     
     # GAME LOOP
     while running:
@@ -89,20 +98,28 @@ def main():
 
         # Object Rendering
         screen.fill('white')
+        pygame.Surface.blit(screen, background_img)
+
+        # Enemy Screen Traversal Test
+        pygame.draw.circle(screen, "white", (target_1), 100)
+        target_1.x += 200 * dt
+        if(target_1.x > screen.get_width() + 120):
+            target_1.x = 0
+
         for ball in ball_cluster:
             pygame.draw.circle(screen, "purple", ball, 10)
-
+        
         # Controller Logic
         dt = clock.tick(60) / 1000
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] or keys[pygame.K_UP]:
-            update_vector_cluster_position_up(ball_cluster, 300, dt)
+            update_vector_cluster_position_up(ball_cluster, 400, dt)
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            update_vector_cluster_position_down(ball_cluster, 300, dt)
+            update_vector_cluster_position_down(ball_cluster, 400, dt)
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            update_vector_cluster_position_left(ball_cluster, 300, dt)
+            update_vector_cluster_position_left(ball_cluster, 400, dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            update_vector_cluster_position_right(ball_cluster, 300, dt)
+            update_vector_cluster_position_right(ball_cluster, 400, dt)
             
         # Object Render Update
         pygame.display.flip()
